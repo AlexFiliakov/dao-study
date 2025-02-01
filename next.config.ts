a70c60a9ef403bac1fc43d8ed0719f6d42387989
Alex Filiakov <alexfiliakov@gmail.com>
2025-02-01 09:06:53 -0500

    config for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/dao'
+  basePath: '/dao',
+  output: 'export',  // This tells Next.js to export static files
 };
 
 export default nextConfig;
