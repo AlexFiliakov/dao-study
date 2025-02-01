ea383e244fcd68972e733530071e2fb644ea4d31
Alex Filiakov <alexfiliakov@gmail.com>
2025-02-01 10:01:57 -0500

    tweak image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',  // This tells Next.js to export static files
-  images: {
-    unoptimized: true,  // Required for static export
-  },
+  // images: {
+  //   unoptimized: true,  // Required for static export
+  // },
 };
 
 export default nextConfig;
