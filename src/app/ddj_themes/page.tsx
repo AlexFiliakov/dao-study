74fe1692b0aa3a08330793c5767dcc032ad2276a
Alex Filiakov <alexfiliakov@gmail.com>
2025-02-02 10:22:49 -0500

    reordered themes

--- src/app/ddj_themes/page.tsx
+++ src/app/ddj_themes/page.tsx
@@ -40,23 +40,26 @@ export default function DDJThemes () {
                     <h2 className="text-xl text-neutral-800">Recurring Themes</h2>
                 </div> */}
                 道 (dào) – &ldquo;The Way&rdquo; or &ldquo;the Path&rdquo;; central to Daoist philosophy.<br /><br />
-                德 (dé) – &ldquo;Virtue&rdquo; or &ldquo;Power&rdquo;; represents the active expression of the Dao.<br /><br />
+                德 (dé) – &ldquo;Virtue&rdquo; or &ldquo;Power&rdquo;; represents the active expression of the Dao.
+            </div>
+            <div className="bg-white mt-8 p-6 rounded-lg shadow-md border-l-4 border-teal-700">
                 大 (dà) – &ldquo;Great&rdquo; or &ldquo;vast&rdquo;; frequently used to describe the Dao and the natural order.<br /><br />
                 常 (cháng) - &ldquo;Constant,&rdquo; &ldquo;eternal,&rdquo; &ldquo;unchanging,&rdquo; or &ldquo;in accordance with natural law.&rdquo; It is a key concept in Daoism, emphasizing the unchanging flow of the Dao.<br /><br />
                 不 (bù) – &ldquo;Not&rdquo; or &ldquo;no&rdquo;; frequently used in negations.
             </div>
             <div className="bg-white mt-8 p-6 rounded-lg shadow-md border-l-4 border-teal-700">
-                有 (yǒu) – &ldquo;Being&rdquo; or &ldquo;existence&rdquo;; often contrasted with wú to express the interplay of opposites.<br /><br />
                 下 (xià) – &ldquo;Low&rdquo; or &ldquo;below&rdquo;; often used metaphorically (e.g., rivers flowing downward).<br /><br />
                 知 (zhī) - &ldquo;To know,&rdquo; &ldquo;knowledge,&rdquo; or &ldquo;wisdom,&rdquo; and it plays a crucial role in Daoist philosophy, often in contrast with deeper understanding or intuitive knowing.
             </div>
             <div className="bg-white mt-8 p-6 rounded-lg shadow-md border-l-4 border-teal-700">
                 生 (shēng) – &ldquo;Life&rdquo; or &ldquo;to give birth&rdquo;; central to Daoist ideas of nature and existence.<br /><br />
-                死 (sǐ) – &ldquo;Death&rdquo;; contrasts with shēng in discussions of the natural cycle.
+                死 (sǐ) – &ldquo;Death&rdquo;; contrasts with shēng in discussions of the natural cycle.<br /><br />
+                道生 (dào shēng) – &ldquo;The Dao gives birth&rdquo;; a recurring phrase about creation.
             </div>
             <div className="bg-white mt-8 p-6 rounded-lg shadow-md border-l-4 border-teal-700">
                 天 (tiān) – &ldquo;Heaven&rdquo; or &ldquo;Nature&rdquo;; often represents the cosmic order.<br /><br />
-                地 (dì) – &ldquo;Earth&rdquo;; complements tiān in describing the natural world.
+                地 (dì) – &ldquo;Earth&rdquo;; complements tiān in describing the natural world.<br /><br />
+                天道 (tiān dào) – &ldquo;The Way of Heaven&rdquo;; a naturalistic concept in Daoism.
             </div>
             <div className="bg-white mt-8 p-6 rounded-lg shadow-md border-l-4 border-teal-700">
                 自 (zì) – &ldquo;Self&rdquo; or &ldquo;spontaneity&rdquo;; related to ziran (自然), meaning &ldquo;naturalness.&ldquo;<br /><br />
@@ -67,11 +70,10 @@ export default function DDJThemes () {
             </div>
             <div className="bg-white mt-8 p-6 rounded-lg shadow-md border-l-4 border-teal-700">
                 无 (wú) – &ldquo;Non-being&rdquo; or &ldquo;without&rdquo;; key to the Daoist idea of emptiness and non-attachment.<br /><br />
+                有 (yǒu) – &ldquo;Being&rdquo; or &ldquo;existence&rdquo;; often contrasted with wú to express the interplay of opposites.<br /><br />
                 为 (wéi) – &ldquo;To act&rdquo; or &ldquo;to do&rdquo;; central to the idea of wu wei (无为, non-action).<br /><br />
                 无为 (wú wéi) – &ldquo;Non-action&rdquo; or &ldquo;effortless action&rdquo;; a foundational Daoist principle.<br />
-                <a href="ddj_ch_16" rel="noopener noreferrer" className="text-red-800 hover:underline">See discussion for Chapter 16.</a><br /><br />
-                道生 (dào shēng) – &ldquo;The Dao gives birth&rdquo;; a recurring phrase about creation.<br /><br />
-                天道 (tiān dào) – &ldquo;The Way of Heaven&rdquo;; a naturalistic concept in Daoism.
+                <a href="ddj_ch_16" rel="noopener noreferrer" className="text-red-800 hover:underline">See notes in Chapter 16.</a>
             </div>
         </div>
       </main>
